83f4d43ed033a9af359a0c3083f95d6926aee52f
fj-x <vihola@protonmail.com>
2025-01-28 13:24:56 +0000

    api main full

--- Infrastructure/api_gateway/main.tf
+++ Infrastructure/api_gateway/main.tf
@@ -20,13 +20,52 @@ resource "aws_api_gateway_resource" "resource_one" {
 
 resource "aws_api_gateway_usage_plan" "usage_plan_one" {
     name = "plan_one"
-    throttle_settings = {
+    throttle_settings {
         burst_limit = 50
         rate_limit = 20
     }
-    quota_settings = {
+    quota_settings {
         limit = 500
         period = "MONTH"
         offset = 0
     }
+}
+
+resource "aws_api_gateway_api_key" "api_key" {
+    name = "first_key"
+    enabled = true
+}
+
+resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
+    key_id = aws_api_gateway_api_key.api_key.id
+    key_type = "API_KEY"
+    usage_plan_id = aws_api_gateway_usage_plan.usage_plan_one
+}
+
+resource "aws_api_gateway_model" "post_model" {
+    rest_api_id = aws_api_gateway_rest_api.rest_api.id
+    name = "PostModel"
+    content_type = "application/json"
+    schema = <<EOF
+    {
+        "$schema": "http://json-scema.org/draft-04/schema#",
+        "title": "Post Scema",
+        "type": "object",
+        "properties": {
+            "first": {"type":"string"},
+            "second": {"type":"boolean"},
+        },
+        "required": [
+            "first",
+            "second"
+        ]
+    }
+    EOF
+}
+
+resource "aws_api_gateway_request_validator" "first_validator" {
+    name = "first_validator"
+    rest_api_id = aws_api_gateway_rest_api.rest_api_id.id
+    validate_request_body = true
+    validate_request_parameters = true
 }
